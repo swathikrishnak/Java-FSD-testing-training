c280e3a9b0c1b2fab8365d8e78ba31f4aa009274
swathikrishnak <94058777+swathikrishnak@users.noreply.github.com>
2022-04-19 10:06:31 +0530

    Add files via upload

---
+++ SpringJDBC/springjdbc/src/main/java/com/ust1/spring/springjdbc/Test.java
@@ -0,0 +1,18 @@
+package com.ust1.spring.springjdbc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("com/ust1/spring/springjdbc/config.xml");
+		JdbcTemplate demo = (JdbcTemplate) ac.getBean("jdbcTemplate");
+		String sql = "insert into employee values(?,?,?)";
+		int res = demo.update(sql, 1, "swathi", "krishna");
+		System.out.println("Number of records inserted:" + res);
+
+	}
+
+}
