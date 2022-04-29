b1bf1091682fe7333a0f8edc332afec67371ad64
swathikrishnak <94058777+swathikrishnak@users.noreply.github.com>
2022-04-29 17:50:04 +0530

    Add files via upload

---
+++ Java 8 features/LambdaExpressionEx.java
@@ -0,0 +1,16 @@
+@FunctionalInterface // optional
+interface MyInterface {
+	public void myMethod();
+}
+
+public class LambdaExpressionEx {
+public static void main(String args[])
+{
+int variable = 10;
+//implemenmtation with lambda expression
+MyInterface myInterface = ()->{
+System.out.println("variable =" +variable);
+};
+myInterface.myMethod();
+}
+}
